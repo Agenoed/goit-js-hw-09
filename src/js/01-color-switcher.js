a07222ac90b898caafbc45a8546ca14c4cd32c37
Oleksandr Deoneha <oleksandr.deoneha@gmail.com>
2022-11-15 21:54:43 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let intervalId = null;
+
+function changeBodyBackground() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.buttonStart.setAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function StopChangeBodyBackground() {
+  clearInterval(intervalId);
+  intervalId = null;
+  refs.buttonStart.removeAttribute('disabled');
+}
+
+refs.buttonStart.addEventListener('click', () => changeBodyBackground());
+refs.buttonStop.addEventListener('click', () => StopChangeBodyBackground());
